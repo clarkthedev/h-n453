e895e659354f8cb2bcef199c0cd4b886cbf33b81
clark calina <clarkcalina@gmail.com>
2017-06-10 01:47:11 +0800

    -change to about us
    \

--- src/components/page-about.vue
+++ src/components/page-about.vue
@@ -1,11 +1,11 @@
 <template>
 	<section class="wrapper">
 		<div class="half-width">
-	<h2>History</h2>
-	<br>
+	<h2>About Us</h2>
 	<p>
+	
 Hainanese delights had its bright start in 2011 at robinsons galleria when our company broadened culinary horizons by branching out into singaporean cuisine. already an advocate of chinese food since late 2007, we continued our passion for asian flavor by bringing singaporean food to the philippines. while chinese and japanese restaurant already have strong footholds in the filipino, singaporean dishes still remain largely unknown. however, the creation of hainanese delights seeks to change that. the cuisine which centers on singaporean’s national dish hainanese chicken appeals to filipino taste being both savory and affordable. but it allso has the exotic singaporean flavor which adds a different twist to the dishes, yet remain affordable price.
-
+<br><br>
 We have a strong history with asian cuisine, having two successful chinese restaurants under our belt. the first ever restaurant chinoy king, put up in december 2017; thereafter, in september 2008, shainghai cuisine was launched to great results. so, already seasoned restaurateurs, we know the ins and outs of building a thriving restaurant. the experience in putting up food business gives us a working knowledge about creating the best restaurant possible. now we set our sights on advancing singaporean food. our new venture hainanese delights benefits from that working knowledge. we have crafted the best restaurant possible, with experienced chefs and staff to provide an excellent dining experience to our valued guests. and with a mindset of quality above anything else, and an adventorous spirit, we hope to give filipino clients more hainanese delights. and with a passion for cooking and a wealth of restaurant experience, the results are a restaurant that has top quality in terms of ambiance, service and most especially food.
 </p>
 </div>
